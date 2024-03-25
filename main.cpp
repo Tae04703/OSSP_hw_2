f152a41c6c472ebb3210049896237e026ce8bb77
TAE0 <mickey470379@gmail.com>
2024-03-25 16:11:23 +0900

    팀이름 입력 및 출력 ossp1에서 수정

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 int main(void){
-	string name, stdID;
+	string name, stdID, team;
 	cout << "이름을 입력하세요: ";
 	cin >> name;
 	cout << "학번을 입력하세요: ";
 	cin >> stdID;
+	cout << "팀이름을 입력하세요: ";
+	cin >> team;
 	cout << endl;
 
 	cout << "<출력>" << endl;
 	cout << "이름: " << name << endl;
 	cout << "학번: " << stdID << endl;
+	cout << "팀이름: " << team << endl;
 	return 0;
 }
